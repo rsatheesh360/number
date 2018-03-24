75d34f728f739af1f4f306683a5925c5a9c25fb5
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-24 14:25:21 +0530

    num

---
+++ .gitignore/number.py
@@ -0,0 +1,11 @@
+def kthno():
+	l=[]
+	(n,k)=map(int,sys.stdin.readline().split())
+	for i in range(n):
+		l.append(int(input()))
+	print(l[k-1])
+try:
+	kthno()
+except:
+	print('invalid')
+  print(n)
